c8c37e4dc94040cee9d65ebe8d3ff57f2356b330
DAVID BISSOU <davidebissou@gmail.com>
2021-12-01 17:12:10 -0600

    app cleanup

--- poke-gambler/src/App.js
+++ poke-gambler/src/App.js
@@ -4,21 +4,12 @@ import Wallet1 from './components/Wallet'
 import CardGenerator from './components/CardGenerator';
 
 
-
-const fetchDitto = () =>{ //fetches ditto sprite url
-  let pokemon = fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-                  .then(response => response.json())
-                  .then(data => console.log(data));
-  //createCard(pokemon);
-}
-
 function App() {
   return (
     <div className="App">
       <h1>PokeDraw</h1>
       <div class="ui divider"></div>
       <CardGenerator/>
-      {/* <Button compact color="blue" onClick= {fetchDitto}>Draw</Button> */}
       <h1>Your Wallet</h1>
       <div class="ui divider"></div>
       <Container>
