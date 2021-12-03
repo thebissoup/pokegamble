f25bd85e6fd8e9bfbc9db119a74329231628eb0c
DAVID BISSOU <davidebissou@gmail.com>
2021-12-03 15:28:33 -0600

    creating image url

--- poke-gambler/src/components/CardGenerator.js
+++ poke-gambler/src/components/CardGenerator.js
@@ -3,12 +3,16 @@ import {Card, Icon, Button} from 'semantic-ui-react';
 
 
 const extra = (
-    <a>
-      <Icon name='bitcoin' />
-      0.000000
-      <Button compact color="blue" floated="right">2D</Button>
-      <Button compact color="green" floated="right">3D</Button>
-    </a> 
+    
+    <div>
+        <a>
+            <Icon name='bitcoin' />
+            0.000000
+        </a>
+        <Button compact color="blue" floated="right">2D</Button> {/*onclick set image to png to */}
+        <Button compact color="green" floated="right">3D</Button> {/*onclick set image to gif */}
+    </div>
+    
   )
 
 const pokemonNames = ["ditto","pikachu","bulbasaur","charmander","mew","omanyte"];
@@ -20,17 +24,23 @@ class CardGenerator extends Component{
             image:'https://projectpokemon.org/images/shiny-sprite/charizard.gif',
             header: 'Null',
             meta: 'None',
-            description: 'A blank and empty card...'
+            description: 'A blank and empty card...',
+            gif: null,
+            png: null
+            //  replace image with png, add a gif property for 3d, and create image property
         }
         this.fetchPokemon = this.fetchPokemon.bind(this);  
         this.getRandoName = this.getRandoName.bind(this);
         this.getRandoPokemon = this.getRandoPokemon.bind(this);
-
+        this.fetch3DPokemonGif = this.fetch3DPokemonGif.bind(this);
     }
+
     getRandoPokemon(){
        let randomName =  this.getRandoName();
-       this.fetchPokemon(randomName);
+       this.fetchPokemon(randomName); 
+       this.fetch3DPokemonGif(randomName);// function that fetches the 3d gif of the pokemon
     }
+
     getRandoName(){ // generate a different random pokemon number each time
         let randomNumber;
         do{
@@ -40,6 +50,7 @@ class CardGenerator extends Component{
         
         return pokemonNames[randomNumber];
     }
+
     async fetchPokemon(name){
 
         let url = "https://pokeapi.co/api/v2/pokemon/" + name;
@@ -52,23 +63,32 @@ class CardGenerator extends Component{
             return string.charAt(0).toUpperCase() + string.slice(1)
         };
 
-
         this.setState({ //setState of card properties
-            image:pokemon.sprites.front_default,
+            image:pokemon.sprites.front_default, // ok for the default
             header:labelName(pokemon.species.name),
             meta:labelName(pokemon.types[0].type.name)
+
+            //create a state called png to store the sprite png
         })
+    }
 
-
+    fetch3DPokemonGif(name){
+        let imageurl = "https://projectpokemon.org/images/normal-sprite/" + name +".gif";
+        this.setState({
+            gif: imageurl
+        })
+        console.log(this.state.gif);
 
     }
+
+
 
     render(){
         return(
             <div className = "one column ui centered grid">
                 <div style={{visibility: 'visible', paddingTop: 20}}>
                     <Card
-                        image={this.state.image}
+                        image={this.state.image} 
                         header={this.state.header}
                         meta={this.state.meta}
                         description={this.state.description}
