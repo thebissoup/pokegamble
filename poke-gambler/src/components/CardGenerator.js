0389ab5c7b174ef61ae8819b1b6e6d3389b16512
DAVID BISSOU <davidebissou@gmail.com>
2021-12-03 22:14:30 -0600

    final fetches

--- poke-gambler/src/components/CardGenerator.js
+++ poke-gambler/src/components/CardGenerator.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Card, Icon, Button} from 'semantic-ui-react';
+import {Card, Icon, Button, Image} from 'semantic-ui-react';
 
 
 const extra = (
@@ -9,13 +9,17 @@ const extra = (
             <Icon name='bitcoin' />
             0.000000
         </a>
-        <Button compact color="blue" floated="right">2D</Button> {/*onclick set image to png to */}
-        <Button compact color="green" floated="right">3D</Button> {/*onclick set image to gif */}
     </div>
     
   )
 
-const pokemonNames = ["ditto","pikachu","bulbasaur","charmander","mew","omanyte"];
+const pokemonNames = ["bulbasaur","ivysaur","venusaur","charmander","charmeleon","charizard","squirtle","wartortle","blastoise","caterpie","metapod","butterfree","weedle","kakuna","beedrill","pidgey","pidgeotto","pidgeot","rattata","raticate","spearow","fearow","ekans","arbok","pikachu","raichu", "sandshrew","sandslash","nidoran",
+"nidorina","nidoqueen","nidoran","nidorino","nidoking","clefairy","clefable","vulpix","ninetales","jigglypuff","wigglytuff","zubat","golbat","oddish","gloom","vileplume","paras", "parasect","venonat","venomoth","diglett","dugtrio","meowth","persian","psyduck","golduck","mankey","primeape",
+"growlithe","arcanine","poliwag","poliwhirl","poliwrath","abra","kadabra","alakazam","machop","graveler","golem","ponyta","rapidash","slowpoke","slowbro",
+"machoke","machamp","bellsprout","weepinbell","victreebel","tentacool","tentacruel","geodude","magnemite","magneton","farfetch'd","doduo","dodrio","seel","dewgong","grimer","muk","shellder","cloyster","gastly","haunter","gengar","onix","drowzee","hypno","krabby","kingler","voltorb","electrode","exeggcute","exeggutor","cubone","marowak","hitmonlee","hitmonchan","lickitung","koffing","weezing","rhyhorn","rhydon","chansey","tangela","kangaskhan","horsea","seadra","goldeen","seaking","staryu","starmie","scyther","jynx","electabuzz","magmar","pinsir","tauros","magikarp","gyarados","lapras","ditto","eevee","vaporeon","jolteon","flareon","porygon",
+"omanyte","omastar","kabuto","kabutops","aerodactyl","snorlax","articuno","zapdos","moltres","dratini","dragonair","dragonite","mewtwo","mew"
+];
+
 let lastNum = -1;
 class CardGenerator extends Component{
     constructor(props){
@@ -33,12 +37,13 @@ class CardGenerator extends Component{
         this.getRandoName = this.getRandoName.bind(this);
         this.getRandoPokemon = this.getRandoPokemon.bind(this);
         this.fetch3DPokemonGif = this.fetch3DPokemonGif.bind(this);
+    
     }
 
     getRandoPokemon(){
        let randomName =  this.getRandoName();
-       this.fetchPokemon(randomName); 
        this.fetch3DPokemonGif(randomName);// function that fetches the 3d gif of the pokemon
+       this.fetchPokemon(randomName); 
     }
 
     getRandoName(){ // generate a different random pokemon number each time
@@ -47,14 +52,13 @@ class CardGenerator extends Component{
             randomNumber = Math.floor(Math.random()*pokemonNames.length); //set to random num in range
         }while(lastNum == randomNumber); //if the last number it was is equal randomNumber repeat
         lastNum = randomNumber;
-        
         return pokemonNames[randomNumber];
     }
 
     async fetchPokemon(name){
-
         let url = "https://pokeapi.co/api/v2/pokemon/" + name;
         let pokemon;
+
         await fetch(url) //fetch the pokemon data
                 .then(response => response.json())
                 .then(data => pokemon = data);
@@ -66,10 +70,11 @@ class CardGenerator extends Component{
         this.setState({ //setState of card properties
             image:pokemon.sprites.front_default, // ok for the default
             header:labelName(pokemon.species.name),
-            meta:labelName(pokemon.types[0].type.name)
-
-            //create a state called png to store the sprite png
+            meta:labelName(pokemon.types[0].type.name),
+            png:pokemon.sprites.front_default
         })
+
+        
     }
 
     fetch3DPokemonGif(name){
@@ -77,15 +82,11 @@ class CardGenerator extends Component{
         this.setState({
             gif: imageurl
         })
-        console.log(this.state.gif);
-
     }
-
-
 
     render(){
         return(
-            <div className = "one column ui centered grid">
+            <div className = "two column ui centered grid">
                 <div style={{visibility: 'visible', paddingTop: 20}}>
                     <Card
                         image={this.state.image} 
@@ -96,6 +97,10 @@ class CardGenerator extends Component{
                     />
                     <Button compact color="blue" onClick={this.getRandoPokemon}>Draw</Button>
                 </div>
+                <div className = "centered">
+                    <Image size="medium"src={this.state.gif}/>
+                </div>
+                
             </div>
         )
     }
