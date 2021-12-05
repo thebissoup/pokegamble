5d69505b3a2e8bc4bdd5d0acaf5fdf0c865f7dcd
DAVID BISSOU <davidebissou@gmail.com>
2021-12-05 16:07:31 -0600

    extra data

--- poke-gambler/src/components/Wallet.js
+++ poke-gambler/src/components/Wallet.js
@@ -11,6 +11,9 @@ const extra = (
   </a> 
 )
 
+let cardsData = [{image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png", name: "Ditto", meta: "Normal", price: 0.00000}];
+
+
 const widthStyling = ["ui stackable four column grid","ui stackable three column grid","ui stackable two column grid"]; // changes in grid width
 let walletWidth = widthStyling[0]; // default is four column width
 function Wallet1 (){
@@ -55,32 +58,26 @@ function Wallet1 (){
                 />
             </div>
             <div class="column">
-                <Card
-                    image='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png'
-                    header='Ditto'
-                    meta='Normal'
-                    description='Ditto (メタモン Metamon) is a Normal-type Pokémon introduced in Generation I.'
-                    extra={extra}
-                />
+            <div className="card-special">
+                    <div className="image-offset" >
+                        <img className='sprite' src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png'/>
+                    </div>
+                    <h1 className="name">Ditto</h1>
+                    <p className="sub">Normal</p>
+                    <div className="card-bar">
+                        <div>
+                        <Button className="card-button" compact color="green">List</Button>
+                        <Button className="card-button" compact >Trade</Button> 
+                        </div>
+                        <a> 
+                        <Icon name='bitcoin' />
+                        0.000000
+                        </a>
+                    </div>
+                </div>
             </div>
-            <div class="column">
-                <Card
-                    image='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png'
-                    header='Ditto'
-                    meta='Normal'
-                    description='Ditto (メタモン Metamon) is a Normal-type Pokémon introduced in Generation I.'
-                    extra={extra}
-                />
-            </div>
-            <div class="column">
-                <Card
-                    image='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png'
-                    header='Ditto'
-                    meta='Normal'
-                    description='Ditto (メタモン Metamon) is a Normal-type Pokémon introduced in Generation I.'
-                    extra={extra}
-                />
-            </div>
+           
+            
         </div>
         
         )
