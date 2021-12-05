7eb757f7f7ca53023ba804afc878f3bc16960fa3
DAVID BISSOU <davidebissou@gmail.com>
2021-12-05 15:42:33 -0600

    final card ui

--- poke-gambler/src/components/CardDisplay.js
+++ poke-gambler/src/components/CardDisplay.js
@@ -1,61 +1,62 @@
 import React, {Component} from "react";
-import { Card, Icon, Button, Image } from "semantic-ui-react";
+import {Icon, Button } from "semantic-ui-react";
 
 class CardDisplay extends Component{
     constructor(props){
         super(props);
 
         this.state ={
-         imageWidth: null,
-         imageHeight:null,
-         paddingLeft: null,
-         paddingTop: null
+         SpriteStyling: "sprite",
+         ArStyling: "ar",
+         currentStyling:"sprite"
         }
-
-        this.resizeImage = this.resizeImage.bind(this);
+        this.arStyle = this.arStyle.bind(this);
+        this.spriteStyle = this.spriteStyle.bind(this);
 
     }
-    resizeImage(image){
-        let width = image.offsetWidth;
-        let height = image.offsetHeight;
+    arStyle(){
         this.setState({
-            imageWidth: width,
-            imageHeight: height,
-            paddingLeft: Math.floor((290 - width)/2),
-            paddingTop: Math.floor((290 - height)/2)
+            currentStyling: this.state.ArStyling
         })
     }
-    
+    spriteStyle(){
+        this.setState({
+            currentStyling: this.state.SpriteStyling
+        })
+    }
 
     
     render(){
         return(
-            <div>
-                <Card>
-                    <Image style={{width:290, height:290, paddingLeft:this.state.paddingLeft, paddingTop:this.state.paddingTop}} wrapped ui={false}>
-                        <img style={{width:this.state.imageWidth, height:this.state.imageHeight}} src={this.props.pokeData.image}/>
-                    </Image>
-                    <Card.Content>
-                        <Card.Header>{this.props.pokeData.header}</Card.Header>
-                        <Card.Meta>{this.props.pokeData.meta}</Card.Meta>
-                        <Card.Description>{this.props.pokeData.description}</Card.Description>
-                    </Card.Content>
-                    <Card.Content extra>
-                    <div>
-                        <a>
-                            <Icon name='bitcoin' />
-                            0.000000
-                        </a>
-                        <Button compact color="blue" floated="right" onClick={this.props.to2D}>2D</Button>
-                        <Button compact color="green" floated="right" onClick={() => {
-                            this.props.to3D();
-                            this.resizeImage(document.getElementsByClassName('imagex')[0])
-                            }}>3D</Button>
+            
+            <div className="side-by-side">
+                
+                
+
+                <div className="card-special">
+                    <div className="image-offset" >
+                        <img className={this.state.currentStyling} src={this.props.pokeData.image}/>
                     </div>
-                    </Card.Content>
-                </Card>
-                <img className='imagex' src={this.props.pokeData.image} style={{visibility:"hidden", position: "absolute"}}></img>
-            </div>
+                    <h1 className="name">{this.props.pokeData.header}</h1>
+                    <p className="sub">{this.props.pokeData.meta}</p>
+                    <div className="card-bar">
+                        <div>
+                        <Button className="card-button" compact color="green" onClick={() =>{
+                            this.props.to3D()
+                            this.arStyle();
+                            }}>3D</Button>
+                        <Button className="card-button" compact color="blue" onClick={() =>{
+                            this.props.to2D();
+                            this.spriteStyle();
+                            }}>2D</Button> 
+                        </div>
+                        <a> 
+                        <Icon name='bitcoin' />
+                        0.000000
+                        </a>
+                    </div>
+                </div>
+        </div>
     
         )
     }
